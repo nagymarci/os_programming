e37174fb7b4f8896779ad5bc44a80c6868e5f4d9
nagymarci <nagy90marci@gmail.com>
2016-09-07 18:36:53 +0200

    Improve build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,34 @@
-i686-elf-as boot.s -o boot.o
-i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+if i686-elf-as boot.s -o boot.o; then
+  echo building boot.s successful
+else
+  echo building boot.s FAILED
+  exit 1
+fi
+if i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra; then
+  echo building kernel successful
+else
+  echo building kernel FAILED
+  exit 2
+fi
+if i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc; then
+  echo linking succesful
+else
+  echo linking FAILED
+  exit 3
+fi
 
 if grub-file --is-x86-multiboot myos.bin; then
   echo multiboot confirmed
 else
   echo the file is not multiboot
-  exit
+  exit 4
 fi
 
 cp myos.bin isodir/boot/
 
-grub-mkrescue -o myos.iso isodir
+if grub-mkrescue -o myos.iso isodir; then
+  echo iso generation successful
+else
+  echo iso generation FAILED
+  exit 5
+fi
